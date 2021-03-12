7c7a142f49ad8e122eccb9aa77af800d08fe8bb7
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2021-03-12 13:12:37 -0600

    Create Debug9-2_BreakfastProgram.cs

---
+++ Debug9-2_BreakfastProgram.cs
@@ -0,0 +1,36 @@
+// Creates a Breakfast class
+// and instantiates an object
+// Displays Breakfast special information
+using System;
+using static System.Console;
+using System.Globalization;
+class DebugNine2
+{
+   static void Main()
+   {
+      Breakfast special = new Breakfast("French toast", 4.99);
+     
+      //Display the info about breakfast
+      WriteLine(Breakfast.INFO);
+      // then display today's special
+      WriteLine("Today we are having {0} for {1}",
+         special.Name, special.Price.ToString("C2", CultureInfo.GetCultureInfo("en-US")));
+  }
+}
+class Breakfast
+{
+   public static string INFO = 
+      "Breakfast is the most important meal of the day.";
+
+  public string Name {get; set;}
+  public double Price {get; set;}
+      
+   // Breakfast constructor requires a
+   // name, e.g "French toast", and a price
+   public Breakfast(string name, double price)
+   {
+       Name = name;
+       Price = price;
+   }
+   
+}
